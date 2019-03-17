ea9fbb8d2e5c3edbc25af84cd9a84c7b8907e8eb
vianney <vianney.plotton@gmail.com>
2019-03-17 21:28:20 +0100

    Actually throw in execute.

--- lys/sql/execute.cpp
+++ lys/sql/execute.cpp
@@ -6,19 +6,19 @@ void lys::core::sql::execute(sqlite3 * db, std::string stmt)
 {
     fmt::print("{}\n", stmt);
     auto err = sqlite3_exec(db, stmt.c_str(), 0, 0, 0);
-    if (err != SQLITE_OK) std::runtime_error(sqlite3_errstr(err));
+    if (err != SQLITE_OK) throw std::runtime_error(sqlite3_errstr(err));
 }
 
 void lys::core::sql::prepare(sqlite3 * db, std::string stmt, sqlite3_stmt ** res)
 {
     fmt::print("{}\n", stmt);
     auto err = sqlite3_prepare_v2(db, stmt.c_str(), -1, res, 0);
-    if (err != SQLITE_OK) std::runtime_error(sqlite3_errstr(err));
+    if (err != SQLITE_OK) throw std::runtime_error(sqlite3_errstr(err));
 }
 
 void lys::core::sql::execute(sqlite3 * db, std::string stmt, int (*callback)(void *, int, char **, char **))
 {
     fmt::print("{}\n", stmt);
     auto err = sqlite3_exec(db, stmt.c_str(), callback, 0, 0);
-    if (err != SQLITE_OK) std::runtime_error(sqlite3_errstr(err));
+    if (err != SQLITE_OK) throw std::runtime_error(sqlite3_errstr(err));
 }
